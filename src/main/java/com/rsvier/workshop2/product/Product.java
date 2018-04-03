ee143fe528c171597489d21983dcc6cd3a81f339
Geert Lamers <wickeh@gmail.com>
2018-04-03 22:31:30 +0200

    product POJO with JPA annotations
    not working yet > DAO calls resulting in NPE's; debugging

--- src/main/java/com/rsvier/workshop2/product/Product.java
+++ src/main/java/com/rsvier/workshop2/product/Product.java
@@ -2,9 +2,20 @@ package com.rsvier.workshop2.product;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
 
+	@Id
+	@Column(name = "product_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long productId;
+	
 	private String productName;
 	private BigDecimal price;
 	private int stockQuantity;
