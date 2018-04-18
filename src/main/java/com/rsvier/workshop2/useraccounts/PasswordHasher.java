0266e4aeb9f8294756e976171e8967c59250141b
OnneDallinga <31065167+OnneDallinga@users.noreply.github.com>
2018-04-18 14:55:41 +0200

    Removed obsolete methods

--- src/main/java/com/rsvier/workshop2/useraccounts/PasswordHasher.java
+++ src/main/java/com/rsvier/workshop2/useraccounts/PasswordHasher.java
@@ -7,9 +7,6 @@ import java.security.SecureRandom;
 public class PasswordHasher {
 
 		    private SecureRandom random;
-		    private String password;
-		    private String encryptedPass;
-		    private String encryptedTotal;
 
 		    public String generateSalt() {
 		        this.random = new SecureRandom();
@@ -17,21 +14,6 @@ public class PasswordHasher {
 		        this.random.nextBytes(bytes);
 		        return new String (bytes);
 		    }
-
-		    public String makeSaltedPasswordHash(String password) {
-		        String saltString = generateSalt();
-		        this.password = password + saltString;
-		        try {
-		            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-		            messageDigest.update(this.password.getBytes());
-		            this.encryptedPass = new String(messageDigest.digest());
-		            this.encryptedTotal = this.encryptedPass + saltString;
-		        } catch (NoSuchAlgorithmException e) {
-		            System.out.println(e);
-		        }
-		        return this.encryptedTotal;
-		    }
-		    
 		    
 		    public String makeSaltedPasswordHash(String password, String salt) {
 		    	String encryptedTotal = password + salt;
@@ -45,17 +27,4 @@ public class PasswordHasher {
 		        }
 		        return encryptedPassword;
 		    }
-
-		    public String hasher(String password) {
-		        this.password = password;
-		        try {
-		            MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-		            messageDigest.update(password.getBytes());
-		            this.encryptedPass = new String(messageDigest.digest());
-		        } catch (NoSuchAlgorithmException e) {
-		            System.out.println(e);
-		        }
-		        return this.encryptedPass;
-		    }
-			
 }
