5cfa2b8c3db1f326202f14737804279bf88828b2
OnneDallinga <31065167+OnneDallinga@users.noreply.github.com>
2018-04-30 21:26:43 +0200

    Missed a new controller method

--- src/main/java/com/rsvier/workshop2/useraccounts/UserCreationController.java
+++ src/main/java/com/rsvier/workshop2/useraccounts/UserCreationController.java
@@ -32,7 +32,7 @@ public class UserCreationController extends Controller {
 	@Override
 	public void runView() {
 		currentMenu.displayMessage();
-		nextController = new LoginController();
+		nextController = loginController;
 		jointAccountAndCustomerCreator();
 	}
 
