60e6ea31f80e7818c4ea91889243aeab6455e552
Geert Lamers <wickeh@gmail.com>
2018-05-01 11:58:38 +0200

    made (currently unused) controller spring-ready

--- src/main/java/com/rsvier/workshop2/controller/UserMainMenuController.java
+++ src/main/java/com/rsvier/workshop2/controller/UserMainMenuController.java
@@ -1,24 +1,26 @@
 package com.rsvier.workshop2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Component;
 
 import com.rsvier.workshop2.customer.CustomerController;
-import com.rsvier.workshop2.customer.CustomerView;
 import com.rsvier.workshop2.order.OrderController;
-import com.rsvier.workshop2.order.OrderView;
 import com.rsvier.workshop2.product.ProductController;
-import com.rsvier.workshop2.product.ProductView;
 import com.rsvier.workshop2.useraccounts.UserMainMenuView;
-import com.rsvier.workshop2.view.*;
 
 @Component
 @ComponentScan
 public class UserMainMenuController extends Controller {
 	
-	public UserMainMenuController(View<UserMainMenuView> currentMenu) {
-		this.currentMenu = currentMenu;
-	}
+	@Autowired
+	private UserMainMenuView currentMenu;
+	@Autowired
+	private CustomerController customerController;
+	@Autowired
+	private ProductController productController;
+	@Autowired
+	private OrderController orderController;
 
 	@Override
 	public void runView() {
@@ -29,15 +31,15 @@ public class UserMainMenuController extends Controller {
 			// If input was 0 the View.askUserForInput method handles elegant program exit
 			switch(userMenuChoice) {
 				// User chose to manage customers
-				case 1: nextController = new CustomerController();
+				case 1: nextController = customerController;
 						validChoice = true;
 						break;
 				// User chose to manage products
-				case 2: nextController = new ProductController() ;
+				case 2: nextController = productController;
 						validChoice = true;
 						break;
 				// User chose to manage orders
-				case 3: nextController = new OrderController();
+				case 3: nextController = orderController;
 						validChoice = true;
 						break;
 				default: System.out.println("Not a valid option.");
